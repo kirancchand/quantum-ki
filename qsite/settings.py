99e3bac6ccca0afa277a3c09a41a1cd3ee9626b1
kiran c chand <kirancchand@gmail.com>
2023-09-08 16:48:34 +0530

    push with changes

--- qsite/settings.py
+++ qsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '2ua+ctpp$bu&uxxa=#s3lch+$$@4o&h=yw_qwkmz+_(#=y1iqr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
